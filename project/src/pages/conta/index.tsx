5a39c8d9c223ad8f6edc6996e24fca3ff668fdf4
dhomini-rabelo <dhominisalgado@gmail.com>
2022-06-22 00:26:13 -0300

    ad /conta

--- project/src/pages/conta/index.tsx
+++ project/src/pages/conta/index.tsx
@@ -1,11 +1,16 @@
 import {Div} from './styles';
 import { AccountPagesModel } from './../../themes/pages/account/index'
+import { SimpleBox } from '../../components/SimpleBox';
+import { Envelope, LockSimple } from 'phosphor-react';
 
 
 export function AccountPage() {
   return (
     <AccountPagesModel>
-      <Div.box></Div.box>
+      <Div.container>
+        <SimpleBox icon={<Envelope size={40} color="#fff" />} title='Alterar email' description='Altere o endereço de email da sua conta' />
+        <SimpleBox icon={<LockSimple size={40} color="#fff" />} title='Alterar senha' description='Altere a senha da sua conta' />
+      </Div.container>
     </AccountPagesModel>
   );
 }
--- project/src/pages/conta/styles.ts
+++ project/src/pages/conta/styles.ts
@@ -1,14 +1,12 @@
 import styled from 'styled-components';
 
 export const Div = {
-  box: styled.div`
-    border: 3.2px solid #5ED5A8;
-    width: 100%;
-    max-width: 400px;
-    margin: 0 auto;
-    height: 80px;
-    border-radius: 20px;
-    
-;
+  container: styled.div`
+    padding-top: 2rem;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
   `,
+
+  dummy: styled.div``,
 };
