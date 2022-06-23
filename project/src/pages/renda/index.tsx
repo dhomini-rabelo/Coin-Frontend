fe8dda631ad358ab3c1b9027fcfffd4e8bd86515
dhomini-rabelo <dhominisalgado@gmail.com>
2022-06-22 22:16:19 -0300

    ad base for income box

--- project/src/pages/renda/index.tsx
+++ project/src/pages/renda/index.tsx
@@ -1,3 +1,4 @@
+import { ArrowUp } from 'phosphor-react';
 import { AccountPagesModel } from '../../themes/pages/account';
 import {Div} from './styles';
 
@@ -6,6 +7,33 @@ export function IncomePage() {
   return (
     <AccountPagesModel>
       <Div.container>
+        <Div.box>
+          <Div.title><span>RENDA</span></Div.title>
+          <Div.data>
+            <span>R$ 250,27</span>
+            <Div.percent>
+              <ArrowUp size={16} color='#32D74B' /><span>10%</span>
+            </Div.percent>
+          </Div.data>
+        </Div.box>
+        <Div.box>
+          <Div.title><span>RENDA</span></Div.title>
+          <Div.data>
+            <span>R$ 250,27</span>
+            <Div.percent>
+              <ArrowUp size={16} color='#32D74B' /><span>10%</span>
+            </Div.percent>
+          </Div.data>
+        </Div.box>
+        <Div.box>
+          <Div.title><span>RENDA</span></Div.title>
+          <Div.data>
+            <span>R$ 250,27</span>
+            <Div.percent>
+              <ArrowUp size={16} color='#32D74B' /><span>10%</span>
+            </Div.percent>
+          </Div.data>
+        </Div.box>
       </Div.container>
     </AccountPagesModel>
   );
--- project/src/pages/renda/styles.ts
+++ project/src/pages/renda/styles.ts
@@ -2,6 +2,76 @@ import styled from 'styled-components';
 
 export const Div = {
   container: styled.div`
-  
+    display: flex;
+    flex-wrap: wrap;
+
+    @media(max-width: 600px) {
+      flex-direction: column;
+    }
   `,
+
+  box: styled.div`
+    border: 3.2px solid #32D74B;
+    color: #fff;
+    display: flex;
+    justify-content: space-between;
+    padding: 2px 8% 10px 8%;
+    border-radius: 8px;
+    align-items: center;
+    max-width: 400px;
+    flex: 1;
+    margin: 0 auto;
+    margin-top: 2rem;
+    
+    @media(min-width: 500px) {
+      flex-direction: column;
+      max-width: 320px;
+      flex: 0;
+    }
+
+    @media(max-width: 400px) {
+      flex-direction: column;
+    }
+  `,
+  
+  title: styled.div`
+    font-size: 44px;
+
+    @media(min-width: 500px) {
+      font-size: 64px;
+    }
+  `,
+  
+  data: styled.div`
+    padding-top: 0.8rem;
+    font-size: 36px;
+    display: flex;
+    align-items: top;
+
+    @media(min-width: 500px) {
+      font-size: 48px;
+    }
+
+  `,
+  
+  percent: styled.div`
+      color: #32D74B;
+      font-size: 22px;
+      display: flex;
+      align-items: top;
+      padding-top: 0.4rem;
+      margin-left: 0.4rem;
+      svg {
+        display: inline-block;
+      }
+      @media(min-width: 500px) {
+       font-size: 30px;
+        svg {
+          width: 24px !important;
+          height: 24px !important;
+        }
+      }
+  `,
+
+  dummy: styled.div``,
 };
