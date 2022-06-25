172e1fd5206011890ff4685947a167ab24a1b463
dhomini-rabelo <dhominisalgado@gmail.com>
2022-06-25 20:51:27 -0300

    renew billPage

--- project/src/pages/renda/index.tsx
+++ project/src/pages/renda/index.tsx
@@ -1,16 +1,57 @@
-import { ArrowUp } from 'phosphor-react';
+import { ClockCounterClockwise } from 'phosphor-react';
+import { BillBoxList } from '../../components/BillBoxList';
 import { PercentBox } from '../../components/PercentBox';
 import { AccountPagesModel } from '../../themes/pages/account';
-import {Div} from './styles';
+import {Div, H2, Span} from './styles';
+import {adaptMoney} from '../../_Core/utils/money'
+import { Navigation } from '../../components/Navigation';
+import { Link } from 'react-router-dom';
+import { ArrowDown, ArrowUp } from 'phosphor-react';
 
 
 export function IncomePage() {
+  let incomeValue = 2000
+  let expenseValue = 1500
+  let billValue = incomeValue - expenseValue
+  let percent = 25
+  let percentColor = percent >= 0 ? '#32D74B' : '#FF6B6B'
+  
+  
   return (
     <AccountPagesModel>
+      <Navigation>
+        <Link to='/' style={{color: '#C1C7CD'}}>Renda</Link>
+        <Link to='/'>Dados</Link>
+      </Navigation>
       <Div.container>
-        <PercentBox title="RENDA" value={100.75} percent={12} />
-        <PercentBox title="GANHO" value={200.00} percent={-15} />
-        <PercentBox title="GASTOS" value={99} percent={-2} />
+        <Div.incomeContainer>
+          <Div.income>
+            <H2.income>SALDO ATUAL</H2.income>
+            <Span.value isIncome={billValue >= 0}>{adaptMoney(billValue)}</Span.value>
+          </Div.income>
+          <Div.income className="forPC">
+            <H2.income>GANHOS</H2.income>
+            <Span.value isIncome={true}>{adaptMoney(incomeValue)}</Span.value>
+          </Div.income>
+          <Div.income className="forPC">
+            <H2.income>GASTOS</H2.income>
+            <Span.value isIncome={false}>{adaptMoney(expenseValue)}</Span.value>
+          </Div.income>
+          <Div.arrow>
+            {
+              percent >= 0 ? (
+                <><ArrowUp size={60} color={percentColor} /><span style={{color: percentColor}}>{percent.toFixed(0)}%</span></>
+                ) : (
+                  <><ArrowDown size={60} color={percentColor} /><span style={{color: percentColor}}>{(percent*-1).toFixed(0)}%</span></>
+                )
+            }
+          </Div.arrow>
+        </Div.incomeContainer>
+        <Div.time>
+          <H2.bill>Histórico</H2.bill>
+          <ClockCounterClockwise size={24} color='#fafafa' />
+        </Div.time>
+        <BillBoxList />
       </Div.container>
     </AccountPagesModel>
   );
--- project/src/pages/renda/styles.ts
+++ project/src/pages/renda/styles.ts
@@ -2,13 +2,88 @@ import styled from 'styled-components';
 
 export const Div = { 
   container: styled.div`
-    display: flex;
-    flex-wrap: wrap;
+    padding-inline: 4.5%;
+  `,
 
-    @media(max-width: 600px) {
-      flex-direction: column;
+  income: styled.div`
+    display: flex;
+    flex-direction: column;
+  `,
+
+  time: styled.div`
+    margin-top: 4.2rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `,
+
+  incomeContainer: styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-top: 3.6rem;
+
+    @media(max-width: 768px) {
+      .forPC {
+        display: none;
+      }
+    }
+  `,
+
+  arrow: styled.div`
+    display: flex;
+    align-items: center;
+    span {
+      color: #32D74B;
+      font-size: 54px;
+    }
+
+
+    @media(min-width: 768px) {
+      display: none;
+    }
+
+    @media(max-width: 400px) {
+      display: none;
     }
   `,
 
   dummy: styled.div``,
 };
+
+export const H2 = {
+
+  income: styled.h1`
+    color: #fafafa;
+    font-size: 14px;
+    max-width: 70%;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 28px;
+    white-space: nowrap;
+  `,
+
+  bill: styled.h1`
+    font-size: 18px;
+    line-height: 22px;
+    letter-spacing: 0.0264em;
+    font-style: normal;
+    font-weight: 700;
+    color: #fafafa;
+  `,
+
+}
+
+export const Span = {
+
+value: styled.span(({isIncome}: {isIncome: boolean}) => `
+  font-weight: 700;
+  color: ${isIncome ? '#5ED5A8': '#FF6B6B'};
+  font-style: normal;
+  font-weight: 600;
+  font-size: 28px;
+  line-height: 32px;
+  margin-top: 1rem;
+`),
+
+}
