537f33183de32b9a3b1db4318a83da6c76073d80
dhomini-rabelo <dhominisalgado@gmail.com>
2022-06-25 19:07:41 -0300

    ad box item component

---
+++ project/src/components/BillBoxList/components/BillBoxItem/index.tsx
@@ -0,0 +1,16 @@
+import { Clock } from 'phosphor-react';
+import {Div, H2, Span} from './../../styles';
+
+
+export function BillBoxItem({title, description, value, isIncome}: {title: string, description: string, value: number, isIncome: boolean}) {
+    return (
+     <Div.box>
+        <Div.title>
+          <H2.title>{title}</H2.title>
+          <Span.value isIncome={isIncome}>{value > 0 ? `R$ ${value.toFixed(2).replace('.', ',')}`: <Clock size={20} />}</Span.value>
+        </Div.title>
+        <Span.description>{description}</Span.description>
+      </Div.box>
+    )
+
+}
---
+++ project/src/components/BillBoxList/styles.ts
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+
+export const Div = {
+  container: styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+  `,
+
+  box: styled.div`
+    margin-top: 3.2rem;
+    width: 45%;
+    
+    &:first-child {
+      margin-top: 2.6rem;
+    }
+
+    @media(max-width: 768px) {
+      width: 100%;
+    }
+  `,
+
+  title: styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 24px;
+    letter-spacing: 0.0264em;
+  `,
+
+  a: styled.div``,
+};
+
+export const H2 = {
+
+    title: styled.h1`
+      color: #C1C7CD;
+      font-size: 14px;
+      max-width: 70%;
+      overflow-x: hidden;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    `,
+  
+}
+
+export const Span = {
+
+  value: styled.span(({isIncome}: {isIncome: boolean}) => `
+    max-width: 30%;
+    font-weight: 700;
+    color: ${isIncome ? '#5ED5A8': '#FF6B6B'};
+    font-size: 14px;
+  `),
+
+  description: styled.span`
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 24px;
+    letter-spacing: 0.0264em;
+    color: #777777;
+    max-width: 100%;
+    display: block;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  `,
+  
+}
