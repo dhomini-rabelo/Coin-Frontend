ceb959616b074c12948f321b4bc7deed4f55e19b
dhomini-rabelo <dhominisalgado@gmail.com>
2022-06-25 19:08:06 -0300

    Add base for /contas

--- project/src/pages/contas/index.tsx
+++ project/src/pages/contas/index.tsx
@@ -1,12 +1,20 @@
+import { Funnel } from 'phosphor-react';
+import { BillBoxList } from '../../components/BillBoxList';
 import { AccountPagesModel } from '../../themes/pages/account';
-import {Div} from './styles';
+import {Div, H1} from './styles';
 
 
 export function BillPage() {
   return (
     <AccountPagesModel>
       <Div.container>
+        <Div.title>
+          <H1.title>Contas</H1.title>
+          <Funnel size={24} color='#fafafa' />
+        </Div.title>
+        <BillBoxList />
       </Div.container>
+
     </AccountPagesModel>
   );
 }
--- project/src/pages/contas/styles.ts
+++ project/src/pages/contas/styles.ts
@@ -2,6 +2,28 @@ import styled from 'styled-components';
 
 export const Div = {
   container: styled.div`
-  
+    padding-inline: 4.5%;
   `,
+
+  title: styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `,
+
+  a: styled.div``,
 };
+
+
+export const H1 = {
+
+  title: styled.h1`
+    font-size: 18px;
+    line-height: 22px;
+    letter-spacing: 0.0264em;
+    font-style: normal;
+    font-weight: 700;
+    color: #fafafa;
+  `,
+
+}
