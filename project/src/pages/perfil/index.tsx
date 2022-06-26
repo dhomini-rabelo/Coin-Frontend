6c80266493a0728424bd620420da23409849cf8c
dhomini-rabelo <dhominisalgado@gmail.com>
2022-06-25 21:20:55 -0300

    up accountPage

--- project/src/pages/perfil/index.tsx
+++ project/src/pages/perfil/index.tsx
@@ -1,7 +1,7 @@
 import {Div} from './styles';
 import { AccountPagesModel } from '../../themes/pages/account/index'
 import { SimpleBox } from '../../components/SimpleBox';
-import { Envelope, LockSimple } from 'phosphor-react';
+import { Envelope, LockSimple, Watch } from 'phosphor-react';
 
 
 export function AccountPage() {
@@ -9,6 +9,7 @@ export function AccountPage() {
     <AccountPagesModel>
       <Div.container>
         <SimpleBox icon={<Envelope size={40} color="#fff" />} title='Alterar email' description='Altere o endereço de email da sua conta' />
+        <SimpleBox icon={<Watch size={40} color="#fff" />} title='Alterar envio' description='Altere o horário do envio de alertas por email' />
         <SimpleBox icon={<LockSimple size={40} color="#fff" />} title='Alterar senha' description='Altere a senha da sua conta' />
       </Div.container>
     </AccountPagesModel>
