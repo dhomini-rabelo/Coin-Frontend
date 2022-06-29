540e9de0fd5055bf0a90191a47ccbfde0ffc0160
dhomini-rabelo <dhominisalgado@gmail.com>
2022-06-28 22:58:33 -0300

    add loading and closeButton component

--- project/src/components/Popovers/CloseButton.tsx
+++ project/src/components/Popovers/CloseButton.tsx
@@ -5,7 +5,7 @@ import { X } from 'phosphor-react'
 export function CloseButton() {
     return (
         <Popover.Button className="text-zinc-500 hover:text-zinc-200" title="botão para fechar popup">
-            <X weight="bold" size={24} className="text-white mr-3 mt-3" />
+            <X weight="bold" size={30} className="text-white mr-4 mt-4" />
         </Popover.Button>
     )
 }
