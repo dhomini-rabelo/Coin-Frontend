0a7b91dc2a1ef011ecb900b229c5bfd331a41ccf
dhomini-rabelo <dhominisalgado@gmail.com>
2022-06-28 23:00:16 -0300

    add popover to accountPage

--- project/src/pages/perfil/index.tsx
+++ project/src/pages/perfil/index.tsx
@@ -1,16 +1,88 @@
 import {Div} from './styles';
+import {Div as FormDiv} from '../../themes/styles/form';
 import { AccountPagesModel } from '../../themes/pages/account/index'
 import { SimpleBox } from '../../components/SimpleBox';
+import { Popover } from '@headlessui/react'
 import { Envelope, LockSimple, Watch } from 'phosphor-react';
+import { SimplePopover } from '../../components/Popovers/SimplePopover';
+import { ButtonForm } from '../../themes/styles/form/components/buttons';
 
 
 export function AccountPage() {
   return (
     <AccountPagesModel>
       <Div.container>
-        <SimpleBox icon={<Envelope size={40} color="#fff" />} title='Alterar email' description='Altere o endereço de email da sua conta' />
-        <SimpleBox icon={<Watch size={40} color="#fff" />} title='Alterar envio' description='Altere o horário do envio de alertas por email' />
-        <SimpleBox icon={<LockSimple size={40} color="#fff" />} title='Alterar senha' description='Altere a senha da sua conta' />
+
+
+    <SimplePopover 
+      button={<SimpleBox icon={<Envelope size={40} color="#fff" />} title='Alterar email' description='Altere o endereço de email da sua conta' />}
+    >
+          <FormDiv.form>
+            <FormDiv.fieldGroup>
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" id="email" placeholder="Digite seu email" />
+              <FormDiv.error></FormDiv.error>
+            </FormDiv.fieldGroup>
+            <FormDiv.fieldGroup>
+              <label htmlFor="new-email">Novo Email</label>
+              <input type="email" name="new-email" id="new-email" placeholder="Digite seu novo email" />
+              <FormDiv.error></FormDiv.error>
+            </FormDiv.fieldGroup>
+            <FormDiv.fieldGroup>
+              <label htmlFor="confirm-new-email">Confirmar Email</label>
+              <input type="email" name="confirm-new-email" id="confirm-new-email" placeholder="Confirme seu novo email" />
+              <FormDiv.error></FormDiv.error>
+            </FormDiv.fieldGroup>
+            <ButtonForm>Confirmar</ButtonForm>
+          </FormDiv.form>
+    </SimplePopover>
+    
+    <SimplePopover 
+      button={<SimpleBox icon={<Watch size={40} color="#fff" />} title='Alterar envio' description='Altere o horário do envio de alertas por email' />}
+    >
+          <FormDiv.form>
+            <FormDiv.fieldGroup>
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" id="email" disabled value="test@test.test" />
+              <FormDiv.error></FormDiv.error>
+            </FormDiv.fieldGroup>
+            <FormDiv.fieldGroup>
+              <label htmlFor="">Novo Email</label>
+              <input type="text" placeholder="Digite seu novo email" />
+              <FormDiv.error></FormDiv.error>
+            </FormDiv.fieldGroup>
+            <FormDiv.fieldGroup>
+              <label htmlFor="">Confirmar Email</label>
+              <input type="text" placeholder="Confirme seu novo email" />
+              <FormDiv.error></FormDiv.error>
+            </FormDiv.fieldGroup>
+            <ButtonForm>Confirmar</ButtonForm>
+          </FormDiv.form>
+      </SimplePopover>
+
+      <SimplePopover 
+          button={<SimpleBox icon={<LockSimple size={40} color="#fff" />} title='Alterar senha' description='Altere a senha da sua conta' />}
+        >
+              <FormDiv.form>
+                <FormDiv.fieldGroup>
+                  <label htmlFor="password">Senha</label>
+                  <input type="password" name="password" id="password" placeholder="Digite sua senha" />
+                  <FormDiv.error></FormDiv.error>
+                </FormDiv.fieldGroup>
+                <FormDiv.fieldGroup>
+                  <label htmlFor="new-password">Nova Senha</label>
+                  <input type="password" name="new-password" id="new-password" placeholder="Digite sua nova senha" />
+                  <FormDiv.error></FormDiv.error>
+                </FormDiv.fieldGroup>
+                <FormDiv.fieldGroup>
+                  <label htmlFor="confirm-new-password">Confirmar Senha</label>
+                  <input type="password" name="confirm-new-password" id="confirm-new-password" placeholder="Digite sua nova senha" />
+                  <FormDiv.error></FormDiv.error>
+                </FormDiv.fieldGroup>
+                <ButtonForm>Confirmar</ButtonForm>
+              </FormDiv.form>
+      </SimplePopover>
+
       </Div.container>
     </AccountPagesModel>
   );
