859fa5a3a89c4e87fb8e68c9002af6c8a5ca4259
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2022-11-24 22:38:16 -0300

    add register user API

---
+++ project/src/code/schemas/user.ts
@@ -0,0 +1,22 @@
+import * as zod from 'zod'
+import { DynamicErrors, ErrorMessages } from './support/messages'
+
+export interface RegisterUserSchemaType {
+  username: string
+  password: string
+  confirm_password: string
+}
+
+export const RegisterUserSchema = zod
+  .object({
+    username: zod
+      .string()
+      .min(1, ErrorMessages.REQUIRED)
+      .max(150, DynamicErrors.maxLength(150)),
+    password: zod.string().min(8, DynamicErrors.minLength(8)),
+    confirm_password: zod.string().min(1, ErrorMessages.REQUIRED),
+  })
+  .refine((data) => data.password === data.confirm_password, {
+    message: 'As senhas são diferentes',
+    path: ['confirm_password'], // field with error
+  })
