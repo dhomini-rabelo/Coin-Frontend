c6c985210119ae309d215c58becd520a964f3611
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2022-11-25 22:23:30 -0300

    add dynamic error message

--- project/src/pages/cadastro/index.tsx
+++ project/src/pages/cadastro/index.tsx
@@ -1,10 +1,11 @@
 import { Div } from '../../themes/styles/form';
-import Logo from '../../../assets/global/favicon.svg';
 import { Link } from 'react-router-dom';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { ButtonForm } from '../../themes/styles/form/components/buttons';
 import { RegisterUserSchema, RegisterUserSchemaType } from '../../code/schemas/user';
-import { SubmitErrorHandler, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
+import { Error } from '../../components/Error';
 
 export function RegisterPage() {
   const registerForm = useForm<RegisterUserSchemaType>({
@@ -26,25 +27,43 @@ export function RegisterPage() {
 
   return (
     <Div.container>
-      <div className="logoContainer"><img src={Logo} alt="logo" /></div>
+      <div className="logoContainer"><img src="/core/logo.svg" alt="logo" /></div>
       <Div.form>
         <form onSubmit={handleSubmit(onValidSubmit, onInValidSubmit)}>
           <Div.fieldGroup>
             <label htmlFor="email">Email</label>
             <input type="email" placeholder="Digite seu email" {...register('email')} />
-            <Div.error></Div.error>
+            <span className="min-h-[20px] block">
+              <ErrorMessage
+                errors={errors}
+                name="email"
+                render={({ message }) => <Error message={message} />}
+              />
+            </span>
           </Div.fieldGroup>
           <Div.fieldGroup>
             <label htmlFor="password">Senha</label>
             <input type="password" placeholder="Digite sua senha" {...register('password')} />
-            <Div.error></Div.error>
+            <span className="min-h-[20px] block">
+              <ErrorMessage
+                errors={errors}
+                name="password"
+                render={({ message }) => <Error message={message} />}
+              />
+            </span>
           </Div.fieldGroup>
           <Div.fieldGroup>
-            <label htmlFor="confirm-password">Confirmar Senha</label>
+            <label htmlFor="confirm_password">Confirmar Senha</label>
             <input type="password" placeholder="Confirme sua senha" {...register('confirm_password')} />
-            <Div.error></Div.error>
+            <span className="min-h-[20px] block">
+              <ErrorMessage
+                errors={errors}
+                name="confirm_password"
+                render={({ message }) => <Error message={message} />}
+              />
+            </span>
           </Div.fieldGroup>
-          <ButtonForm>Cadastrar</ButtonForm>
+          <ButtonForm active={true}>Cadastrar</ButtonForm>
           <Div.btnBottom>
             <Link to="/login">Fazer login</Link>
           </Div.btnBottom>
