17a55ab9e43d0bfeaf44668cd2d62d438a01b16b
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2022-11-27 20:24:20 -0300

    add login schema for login page

---
+++ project/src/code/schemas/login.ts
@@ -0,0 +1,12 @@
+import * as zod from 'zod'
+import { ErrorMessages } from './support/messages'
+
+export interface LoginSchemaType {
+  email: string
+  password: string
+}
+
+export const LoginSchema = zod.object({
+  email: zod.string().min(1, ErrorMessages.REQUIRED),
+  password: zod.string().min(1, ErrorMessages.REQUIRED),
+})
--- project/src/pages/login/index.tsx
+++ project/src/pages/login/index.tsx
@@ -1,22 +1,56 @@
-import { Div } from '../../themes/styles/form';
-import { Link } from 'react-router-dom';
-import { ButtonForm } from '../../themes/styles/form/components/buttons';
+import { Div } from '../../themes/styles/form'
+import { Link, useNavigate } from 'react-router-dom'
+import { ButtonForm } from '../../themes/styles/form/components/buttons'
+import { useFeedback } from '../../code/hooks/useFeedback'
+import { useForm } from 'react-hook-form'
+import { LoginSchema, LoginSchemaType } from '../../code/schemas/login'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 export function LoginPage() {
+  // const navigateTo = useNavigate()
+  // const { FeedbackElement, renderFeedback } = useFeedback()
+  // const registerForm = useForm<LoginSchemaType>({
+  //   resolver: zodResolver(LoginSchema),
+  // })
+  // const {
+  //   handleSubmit,
+  //   formState: { errors, isSubmitSuccessful },
+  //   register,
+  //   setError,
+  //   reset,
+  // } = registerForm
   return (
     <Div.container>
-      <div className="logoContainer"><img src="/core/logo.svg" alt="logo" /></div>
+      <div className="logoContainer">
+        <img src="/core/logo.svg" alt="logo" />
+      </div>
       <Div.form>
         <Div.fieldGroup>
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" placeholder="Digite seu email" />
-          <Div.error><img src="/forms/error.svg" alt="error-icon" /><span>Este campo é obrigatório</span></Div.error>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Digite seu email"
+          />
+          <Div.error>
+            <img src="/forms/error.svg" alt="error-icon" />
+            <span>Este campo é obrigatório</span>
+          </Div.error>
         </Div.fieldGroup>
         <Div.fieldGroup>
           <label htmlFor="password">Senha</label>
-          <input type="password" name="password" id="password" placeholder="Digite sua senha" />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Digite sua senha"
+          />
           <Div.forgot>
-            <Div.error><img src="/forms/error.svg" alt="error-icon" /><span>Este campo é obrigatório</span></Div.error>
+            <Div.error>
+              <img src="/forms/error.svg" alt="error-icon" />
+              <span>Este campo é obrigatório</span>
+            </Div.error>
             <a href="#">Esqueceu a senha?</a>
           </Div.forgot>
         </Div.fieldGroup>
@@ -26,5 +60,5 @@ export function LoginPage() {
         </Div.btnBottom>
       </Div.form>
     </Div.container>
-  );
+  )
 }
