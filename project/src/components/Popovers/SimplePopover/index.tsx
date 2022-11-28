3332bbdc382eda74c101f3a9862d448a73b6234f
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2022-11-27 21:53:17 -0300

    eslint for files

--- project/src/components/Popovers/SimplePopover/index.tsx
+++ project/src/components/Popovers/SimplePopover/index.tsx
@@ -1,26 +1,35 @@
-import { Popover, Transition } from '@headlessui/react'
+import { Popover } from '@headlessui/react'
 import { ReactNode } from 'react'
 import { CloseButton } from '../CloseButton'
 import { Div } from './styles'
 
+export function SimplePopover({
+  button,
+  children,
+  long = false,
+}: {
+  button: ReactNode
+  children: ReactNode
+  long?: boolean
+}) {
+  return (
+    <Popover className="flex" style={{ width: '400px' }}>
+      <Popover.Panel>
+        <Div.container>
+          <Div.box long={long}>
+            <Div.header>
+              <CloseButton></CloseButton>
+            </Div.header>
+            {long ? (
+              <div className="long-form">{children}</div>
+            ) : (
+              <div>{children}</div>
+            )}
+          </Div.box>
+        </Div.container>
+      </Popover.Panel>
 
-export function SimplePopover({button, children, long=false}: {button:ReactNode, children:ReactNode, long: boolean}) {
-    return (
-        <Popover className="flex" style={{width: '400px'}}>
-
-        <Popover.Panel>
-            <Div.container>
-                <Div.box long={long}>
-                    <Div.header>
-                        <CloseButton></CloseButton>
-                    </Div.header>
-                        {long ? <div className="long-form">{children}</div> : <div>{children}</div>}
-                </Div.box>
-            </Div.container>
-        </Popover.Panel>
-
-
-        <Popover.Button className="flex-1">{button}</Popover.Button>
-      </Popover>
-    )
+      <Popover.Button className="flex-1">{button}</Popover.Button>
+    </Popover>
+  )
 }
--- project/src/pages/perfil/styles.ts
+++ project/src/pages/perfil/styles.ts
@@ -1,12 +1,12 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const Div = {
   container: styled.div`
-    padding-top: 2rem;
+    padding-top: 1.25rem;
     display: flex;
     justify-content: space-around;
     flex-wrap: wrap;
   `,
 
   dummy: styled.div``,
-};
+}
