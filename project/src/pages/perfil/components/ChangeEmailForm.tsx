02fc83337f4820733e6b5aa0ed682f19aab2ac12
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2022-11-28 23:12:54 -0300

    process update email

--- project/src/pages/perfil/components/ChangeEmailForm.tsx
+++ project/src/pages/perfil/components/ChangeEmailForm.tsx
@@ -12,8 +12,12 @@ import {
 } from '../../../code/schemas/profile/changeEmail'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Error } from '../../../layout/components/Error'
+import { client } from '../../../core/settings'
+import { useFeedback } from '../../../code/hooks/useFeedback'
+import { showErrorMessages } from '../../../code/utils/errors'
 
 export function ChangeEmailForm() {
+  const { FeedbackElement, renderFeedback } = useFeedback()
   const {
     auth: { email },
   } = useContext(AuthContext)
@@ -28,52 +32,82 @@ export function ChangeEmailForm() {
     reset,
   } = changeEmailFormHook
 
-  function onValidSubmit(data: ChangeEmailSchemaType) { }
+  function onValidSubmit(data: ChangeEmailSchemaType) {
+    client
+      .put('change-email', {
+        email: data.new_email,
+      })
+      .then((response) => {
+        // setEmail(data.new_email)
+        renderFeedback('success', 'Email alterado com sucesso', () => {
+          // logout()
+        })
+      })
+      .catch((error) => {
+        if (error.response.status === 400) {
+          showErrorMessages<ChangeEmailSchemaType>(
+            error.response.data,
+            data,
+            setError,
+            reset,
+          )
+        } else if (error.response.status === 401) {
+          reset(data)
+          renderFeedback('error', 'Credenciais incorretas')
+        } else {
+          reset(data)
+          renderFeedback('error', 'Server Error')
+        }
+      })
+  }
 
   return (
-    <SimplePopover
-      button={
-        <SimpleBox
-          icon={<Envelope size={40} color="#fff" />}
-          title="Alterar email"
-          description="Altere o endereço de email da sua conta"
-        />
-      }
-    >
-      <FormDiv.form>
-        <form onSubmit={handleSubmit(onValidSubmit)}>
-          <FormDiv.fieldGroup>
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              disabled
-              value={email}
-            />
-            <FormDiv.error></FormDiv.error>
-          </FormDiv.fieldGroup>
-          <FormDiv.fieldGroup>
-            <label htmlFor="new-email">Novo Email</label>
-            <input
-              type="email"
-              placeholder="Digite seu novo email"
-              {...register('new_email')}
-            />
-            <Error field="new_email" errors={errors} />
-          </FormDiv.fieldGroup>
-          <FormDiv.fieldGroup>
-            <label htmlFor="confirm-new-email">Confirmar Email</label>
-            <input
-              type="email"
-              placeholder="Confirme seu novo email"
-              {...register('confirm_new_email')}
-            />
-            <Error field="confirm_new_email" errors={errors} />
-          </FormDiv.fieldGroup>
-          <ButtonForm>Confirmar</ButtonForm>
-        </form>
-      </FormDiv.form>
-    </SimplePopover>
+    <>
+      {FeedbackElement}
+      <SimplePopover
+        button={
+          <SimpleBox
+            icon={<Envelope size={40} color="#fff" />}
+            title="Alterar email"
+            description="Altere o endereço de email da sua conta"
+          />
+        }
+      >
+        <FormDiv.form>
+          <form onSubmit={handleSubmit(onValidSubmit)}>
+            <FormDiv.fieldGroup>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                disabled
+                value={email}
+              />
+              <FormDiv.error></FormDiv.error>
+            </FormDiv.fieldGroup>
+            <FormDiv.fieldGroup>
+              <label htmlFor="new-email">Novo Email</label>
+              <input
+                type="email"
+                placeholder="Digite seu novo email"
+                {...register('new_email')}
+              />
+              <Error field="new_email" errors={errors} />
+            </FormDiv.fieldGroup>
+            <FormDiv.fieldGroup>
+              <label htmlFor="confirm-new-email">Confirmar Email</label>
+              <input
+                type="email"
+                placeholder="Confirme seu novo email"
+                {...register('confirm_new_email')}
+              />
+              <Error field="confirm_new_email" errors={errors} />
+            </FormDiv.fieldGroup>
+            <ButtonForm onFetch={isSubmitSuccessful}>Confirmar</ButtonForm>
+          </form>
+        </FormDiv.form>
+      </SimplePopover>
+    </>
   )
 }
