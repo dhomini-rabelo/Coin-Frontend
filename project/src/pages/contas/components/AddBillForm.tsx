2688e910f6ee73e0c2360edf30d210756523866c
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2022-12-04 22:01:56 -0300

    save bill in backend

--- project/src/pages/contas/components/AddBillForm.tsx
+++ project/src/pages/contas/components/AddBillForm.tsx
@@ -10,14 +10,14 @@ import {
 import { zodResolver } from '@hookform/resolvers/zod'
 import { SelectField } from '../../../layout/themes/components/forms/SelectField'
 import { InputForm } from '../../../layout/themes/components/forms/InputForm'
+import { useFeedback } from '../../../code/hooks/useFeedback'
+import { client } from '../../../core/settings'
+import { processFormErrorResponse } from '../../../code/utils/errors'
 
 export function AddBillForm({ children }: { children: ReactNode }) {
+  const { FeedbackElement, renderFeedback } = useFeedback()
   const registerBillFormHook = useForm<RegisterBillSchemaType>({
     resolver: zodResolver(RegisterBillSchema),
-    defaultValues: {
-      day: 1,
-      partials: 1,
-    },
   })
   const {
     handleSubmit,
@@ -32,14 +32,14 @@ export function AddBillForm({ children }: { children: ReactNode }) {
 
   const currentBillType = watch('bill_type')
 
-  const activeExtraFields = ['scheduled_expense', 'scheduled_income'].includes(
+  const isScheduled = ['scheduled_expense', 'scheduled_income'].includes(
     currentBillType,
   )
 
-  const displayExtraFieldsClass = activeExtraFields ? '' : 'hidden'
+  const displayExtraFieldsClass = isScheduled ? '' : 'hidden'
 
   useEffect(() => {
-    if (!activeExtraFields) {
+    if (!isScheduled) {
       const registerFormState = getValues()
       reset({
         ...registerFormState,
@@ -48,101 +48,125 @@ export function AddBillForm({ children }: { children: ReactNode }) {
         value: registerFormState.value || 1,
       })
     }
-  }, [activeExtraFields, setValue, getValues, reset])
+  }, [isScheduled, setValue, getValues, reset])
 
   function onValidSubmit(data: RegisterBillSchemaType) {
-    console.log(data)
+    const isScheduled = ['scheduled_expense', 'scheduled_income'].includes(
+      data.bill_type,
+    )
+    client
+      .post(
+        'bills',
+        isScheduled ? data : { ...data, day: null, partials: null },
+      )
+      .then((response) => {
+        reset(data)
+        renderFeedback('success', 'Conta adicionada')
+      })
+      .catch((error) => {
+        processFormErrorResponse<RegisterBillSchemaType>(
+          error,
+          data,
+          setError,
+          reset,
+          renderFeedback,
+        )
+      })
   }
 
   return (
-    <SimplePopover button={children} long={true}>
-      <form
-        onSubmit={handleSubmit(onValidSubmit, (formErrors) =>
-          console.log('ERROS:', formErrors),
-        )}
-      >
-        <FormDiv.form className="grid grid-cols-2 gap-x-4">
-          <div className="col-span-1">
-            <InputForm
-              name="title"
-              label="Título"
-              placeholder="Digite o título da conta"
-              errors={errors}
-              register={register}
-            />
-          </div>
-          <div className="col-span-1">
-            <SelectField
-              name="bill_type"
-              label="Tipo"
-              choices={{
-                income: 'Ganho',
-                scheduled_income: 'Ganho agendado',
-                expense: 'Despesa',
-                scheduled_expense: 'Despesa agendada',
-              }}
-              errors={errors}
-              register={register}
-            />
-          </div>
-          <div className="col-span-1">
-            <InputForm
-              name="description"
-              label="Descrição (opcional)"
-              placeholder="Digite a descrição da conta"
-              errors={errors}
-              register={register}
-            />
-          </div>
-          <div className="col-span-1">
-            <SelectField
-              name="payment_method"
-              label="Meio de pagamento"
-              choices={{
-                card: 'Cartão',
-                money: 'Dinheiro',
-                pix: 'PIX',
-                billet: 'Boleto',
-              }}
-              errors={errors}
-              register={register}
-            />
-          </div>
-          <div className={`col-span-1 ${displayExtraFieldsClass}`}>
-            <InputForm
-              type="int"
-              name="partials"
-              label="Parcelas"
-              placeholder="Digite a quantidade de parcelas do pagamento"
-              errors={errors}
-              register={register}
-            />
-          </div>
-          <div className={`col-span-1 ${displayExtraFieldsClass}`}>
-            <InputForm
-              type="int"
-              name="day"
-              label="Dia"
-              placeholder="Digite o dia de pagamento"
-              errors={errors}
-              register={register}
-            />
-          </div>
-          <div className="col-span-1">
-            <InputForm
-              type="decimal"
-              name="value"
-              label="Valor"
-              placeholder="Digite o valor da conta"
-              errors={errors}
-              register={register}
-            />
-          </div>
-          <div className="col-span-1">
-            <ButtonForm>Confirmar</ButtonForm>
-          </div>
-        </FormDiv.form>
-      </form>
-    </SimplePopover>
+    <>
+      {FeedbackElement}
+
+      <SimplePopover button={children} long={true}>
+        <form
+          onSubmit={handleSubmit(onValidSubmit, (formErrors) =>
+            console.log('ERROS:', formErrors),
+          )}
+        >
+          <FormDiv.form className="grid grid-cols-2 gap-x-4">
+            <div className="col-span-1">
+              <InputForm
+                name="title"
+                label="Título"
+                placeholder="Digite o título da conta"
+                errors={errors}
+                register={register}
+              />
+            </div>
+            <div className="col-span-1">
+              <SelectField
+                name="bill_type"
+                label="Tipo"
+                choices={{
+                  income: 'Ganho',
+                  scheduled_income: 'Ganho agendado',
+                  expense: 'Despesa',
+                  scheduled_expense: 'Despesa agendada',
+                }}
+                errors={errors}
+                register={register}
+              />
+            </div>
+            <div className="col-span-1">
+              <InputForm
+                name="description"
+                label="Descrição (opcional)"
+                placeholder="Digite a descrição da conta"
+                errors={errors}
+                register={register}
+              />
+            </div>
+            <div className="col-span-1">
+              <SelectField
+                name="payment_method"
+                label="Meio de pagamento"
+                choices={{
+                  card: 'Cartão',
+                  money: 'Dinheiro',
+                  pix: 'PIX',
+                  billet: 'Boleto',
+                }}
+                errors={errors}
+                register={register}
+              />
+            </div>
+            <div className={`col-span-1 ${displayExtraFieldsClass}`}>
+              <InputForm
+                type="int"
+                name="partials"
+                label="Parcelas"
+                placeholder="Digite a quantidade de parcelas do pagamento"
+                errors={errors}
+                register={register}
+              />
+            </div>
+            <div className={`col-span-1 ${displayExtraFieldsClass}`}>
+              <InputForm
+                type="int"
+                name="day"
+                label="Dia"
+                placeholder="Digite o dia de pagamento"
+                errors={errors}
+                register={register}
+              />
+            </div>
+            <div className="col-span-1">
+              <InputForm
+                type="decimal"
+                name="value"
+                label="Valor"
+                placeholder="Digite o valor da conta"
+                errors={errors}
+                register={register}
+              />
+            </div>
+            <div className="col-span-1">
+              <ButtonForm onFetch={isSubmitSuccessful}>Confirmar</ButtonForm>
+            </div>
+          </FormDiv.form>
+        </form>
+      </SimplePopover>
+    </>
   )
 }
